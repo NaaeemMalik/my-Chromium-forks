16f69a0a27f4b1bb52852c9a5bcde81673db0189
Michael Lippautz <mlippautz@chromium.org>
2022-03-17 10:23:25 +0000

    Settings: Remove incorrect logic from HandleAddStartupPage.
    The code was trying to parse an index argument from the parameters
    passed from JS, but such a parameter is never actually passed from the
    UI. Moreover the code that was handling it was incrorectly not checking
    for "greater than zero" opening up the door unnecessarily to potential
    vulnerabilities, if someose manually passes parameters using the
    DevTools console (or if somehow the WebUI page is compromised).
    
    Also changing DCHECK_GE to CHECK_EQ in HandleRemoveStartupPage.
    
    Bug: 1297429
    Change-Id: I1f26c75c5193813c5b5c62e803520706c38c7791
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3500785
    Auto-Submit: Demetrios Papadopoulos <dpapad@chromium.org>
    Reviewed-by: John Lee <johntlee@chromium.org>
    Commit-Queue: John Lee <johntlee@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#977725}

--- third_party/blink/renderer/platform/heap/persistent.h
+++ third_party/blink/renderer/platform/heap/persistent.h
@@ -33,6 +33,18 @@ using WeakPersistent = cppgc::WeakPersistent<T>;
 template <typename T>
 using CrossThreadPersistent = cppgc::subtle::CrossThreadPersistent<T>;
 
+// CrossThreadWeakPersistent allows weakly retaining objects from threads other
+// than the thread that owns the heap of the corresponding object.
+//
+// Caveats:
+// - Does not protect the heap owning an object from termination, as the
+//   reference is weak.
+// - In order to access the underlying object
+//   `CrossThreadWeakPersistent<T>::Lock()` must be used which returns a
+//   `CrossThreadPersistent<T>` which in turn also does not protect the heap
+//   owning the object from terminating (see above).
+// - Reaching transitively through the graph is unsupported as objects may be
+//   moved concurrently on the thread owning the object.
 template <typename T>
 using CrossThreadWeakPersistent = cppgc::subtle::WeakCrossThreadPersistent<T>;
 
