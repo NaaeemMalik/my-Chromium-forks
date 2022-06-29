6f9ca7cdd1284dde726f307ad7bf3255100aa01b
Arthur Sonzogni <arthursonzogni@chromium.org>
2022-06-29 12:30:47 +0000

    Avoid leaking cross-origin data via document.baseURI. CVE-2022-3057 This patch do not improve document.baseURI implementation. It is still wrong.
    The least we can do is preventing it from leaking cross-origin data.
    
    Bug: 1336904
    Change-Id: I53d23b12134c4c0c7866353a6e09c327a3ac443b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3726194
    Commit-Queue: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1019091}

--- third_party/blink/renderer/core/dom/document.cc
+++ third_party/blink/renderer/core/dom/document.cc
@@ -4104,6 +4104,17 @@ void Document::UpdateBaseURL() {
 }
 
 KURL Document::FallbackBaseURL() const {
+  const bool is_parent_cross_origin =
+      GetFrame() && GetFrame()->IsCrossOriginToParentOrOuterDocument();
+  // TODO(https://crbug.com/751329, https://crbug.com/1336904): Referring to
+  // ParentDocument() is not correct.
+  // We avoid using it when it is cross-origin, to avoid leaking cross-origin.
+  const Document* same_origin_parent =
+      is_parent_cross_origin ? nullptr : ParentDocument();
+
+  // [spec] 1. If document is an iframe srcdoc document, then return the
+  //           document base URL of document's browsing context's container
+  //           document.
   if (IsSrcdocDocument()) {
     // TODO(tkent): Referring to ParentDocument() is not correct.  See
     // crbug.com/751329.
@@ -4112,10 +4123,11 @@ KURL Document::FallbackBaseURL() const {
   } else if (urlForBinding().IsAboutBlankURL()) {
     if (!dom_window_ && execution_context_)
       return execution_context_->BaseURL();
-    // TODO(tkent): Referring to ParentDocument() is not correct.  See
-    // crbug.com/751329.
-    if (Document* parent = ParentDocument())
-      return parent->BaseURL();
+    }
+
+    if (same_origin_parent) {
+      return same_origin_parent->BaseURL();
+    }
   }
   return urlForBinding();
 }
