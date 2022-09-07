7a4b98477fb70e3412727b4eb0724e0607579d74
Yann Dago <ydago@chromium.org>
2022-09-07 10:03:40 +0000

    [M102-LTS] Use CancelableCallback in ForcedProfileSwitchInterceptionHandle to avoid use-after-free
    (cherry picked from commit fec4acb18f371436e6dc34d40b73aff3d7bba037)
    
    Bug: 1341918
    Change-Id: Ic1cce9b149ba38a9b1d02c2d7568a75ecbed674f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3788885
    Auto-Submit: Yann Dago <ydago@chromium.org>
    Commit-Queue: Yann Dago <ydago@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1028784}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/3865007
    Owners-Override: Jana Grill <janagrill@google.com>
    Commit-Queue: Roger Felipe Zanoni da Silva <rzanoni@google.com>
    Reviewed-by: Jana Grill <janagrill@google.com>
    Cr-Commit-Position: refs/branch-heads/5005@{#1339}
    Cr-Branched-From: 5b4d9450fee01f821b6400e947b3839727643a71-refs/heads/main@{#992738}

--- chrome/browser/ui/signin/dice_web_signin_interceptor_delegate.cc
+++ chrome/browser/ui/signin/dice_web_signin_interceptor_delegate.cc
@@ -7,6 +7,7 @@
 #include <memory>
 
 #include "base/callback.h"
+#include "base/cancelable_callback.h"
 #include "base/feature_list.h"
 #include "base/memory/raw_ptr.h"
 #include "build/build_config.h"
@@ -26,11 +27,16 @@ class ForcedProfileSwitchInterceptionHandle
   explicit ForcedProfileSwitchInterceptionHandle(
       base::OnceCallback<void(SigninInterceptionResult)> callback) {
     DCHECK(callback);
+    cancelable_callback_.Reset(std::move(callback));
     base::ThreadTaskRunnerHandle::Get()->PostTask(
-        FROM_HERE, base::BindOnce(std::move(callback),
+        FROM_HERE, base::BindOnce(cancelable_callback_.callback(),
                                   SigninInterceptionResult::kAccepted));
   }
   ~ForcedProfileSwitchInterceptionHandle() override = default;
+
+ private:
+  base::CancelableOnceCallback<void(SigninInterceptionResult)>
+      cancelable_callback_;
 };
 
 class ForcedEnterpriseSigninInterceptionHandle
