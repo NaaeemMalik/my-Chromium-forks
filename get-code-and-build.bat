c5aed6bacfc5d4cfa8bb3ac04f65659619cb04ba
Doug Warner <idougwarner@gmail.com>
2023-04-18 19:54:19 +0500

    fix build mac

--- get-code-and-build.bat
+++ get-code-and-build.bat
@@ -1,11 +1,49 @@
-git clone -b gtx-dev --depth=1 https://github.com/OSITA-Consulting/gtx-browser.git src
+git clone -b gtx-dev --depth=1 https://github.com/OSITA-Consulting/gametransition-browser.git src
 
-git clone -b gtx-dev --depth=1 https://github.com/OSITA-Consulting/gtx-browser.git src
+git clone -b gtx-dev --depth=1 https://github.com/OSITA-Consulting/gametransition-browser.git src
 
 cd src
 
 
 gclient sync
+
+gclient sync
+
+cd v8
+git reset --hard
+git remote add gtx https://github.com/OSITA-Consulting/v8
+git pull gtx gtx
+git switch gtx 
+cd ..
+
+cd net\third_party\quiche\src
+git reset --hard
+git remote add gtx https://github.com/OSITA-Consulting/quiche
+git pull gtx gtx
+git switch gtx 
+cd ..\..
+
+cd angle
+git reset --hard
+git remote add gtx https://github.com/OSITA-Consulting/angle
+git pull gtx gtx
+git switch gtx 
+cd ..
+
+
+cd skia
+git reset --hard
+git remote add gtx https://github.com/OSITA-Consulting/skia
+git pull gtx gtx
+git switch gtx 
+cd ..
+
+cd pdfium
+git reset --hard
+git remote add gtx https://github.com/OSITA-Consulting/PDFium
+git pull gtx gtx
+git switch gtx 
+
 
 gclient sync
 
---
+++ get-code-and-build.sh
@@ -0,0 +1,70 @@
+git clone -b gtx-dev --depth=1 https://github.com/OSITA-Consulting/gametransition-browser.git src
+
+git clone -b gtx-dev --depth=1 https://github.com/OSITA-Consulting/gametransition-browser.git src
+
+cd src
+
+
+gclient sync
+
+gclient sync
+
+cd v8
+git reset --hard
+git remote add gtx https://github.com/OSITA-Consulting/v8
+git pull gtx gtx
+git switch gtx 
+cd ..
+
+cd net/third_party/quiche/src
+git reset --hard
+git remote add gtx https://github.com/OSITA-Consulting/quiche
+git pull gtx gtx
+git switch gtx 
+cd ../..
+
+cd angle
+git reset --hard
+git remote add gtx https://github.com/OSITA-Consulting/angle
+git pull gtx gtx
+git switch gtx 
+cd ..
+
+
+cd skia
+git reset --hard
+git remote add gtx https://github.com/OSITA-Consulting/skia
+git pull gtx gtx
+git switch gtx 
+cd ..
+
+cd pdfium
+git reset --hard
+git remote add gtx https://github.com/OSITA-Consulting/PDFium
+git pull gtx gtx
+git switch gtx 
+
+
+gclient sync
+
+gclient sync
+
+gn gen out/gtx --args="treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true"
+
+gn gen out/gtx --args="treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true"
+
+
+autoninja -C out/gtx chrome
+
+autoninja -C out/gtx chrome
+
+autoninja -C out/gtx chrome
+
+autoninja -C out/gtx chrome
+
+ninja -C out/gtx mini_installer
+
+ninja -C out/gtx mini_installer
+
+
+"out/gtx/gtxbrowser.exe"
