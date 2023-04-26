263ee64b2d62dee038bb0fdeeaa4a6167ab99507
Doug Warner <idougwarner@gmail.com>
2023-04-26 22:52:35 +0500

    gtx-browser  repo

--- fix-build.sh
+++ fix-build.sh
@@ -1,50 +1,19 @@
-@REM ninja -C out/gtx -t clean
-
 gclient sync
-
-cd v8
-git reset --hard
-git remote add gtx https://github.com/OSITA-Consulting/v8
-git pull gtx gtx --rebase
-git switch gtx 
-cd ..
-
-cd net/third_party/quiche/src
-git reset --hard
-git remote add gtx https://github.com/OSITA-Consulting/quiche
-git pull gtx gtx --rebase
-git switch gtx 
-cd ../..
-
-cd angle
-git reset --hard
-git remote add gtx https://github.com/OSITA-Consulting/angle
-git pull gtx gtx --rebase
-git switch gtx 
-cd ..
-
-
-cd skia
-git reset --hard
-git remote add gtx https://github.com/OSITA-Consulting/skia
-git pull gtx gtx --rebase
-git switch gtx 
-cd ..
-
-cd pdfium
-git reset --hard
-git remote add gtx https://github.com/OSITA-Consulting/PDFium
-git pull gtx gtx --rebase
-git switch gtx 
-
-
-gclient runhooks
-
-gn gen out/gtx --args="treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true"
-
-autoninja -C out/gtx chrome
-
-ninja -C out/gtx mini_installer
+rmdir /s /q v8
+git clone -b gtx --depth=1 https://github.com/OSITA-Consulting/v8 v8
+rmdir /s /q net/third_party/quiche/src
+git clone -b gtx --depth=1 https://github.com/OSITA-Consulting/quiche net/third_party/quiche/src 
+rmdir /s /q third_party/angle 
+git clone -b gtx --depth=1 https://github.com/OSITA-Consulting/angle third_party/angle
+rmdir /s /q third_party/skia
+git clone -b gtx --depth=1 https://github.com/OSITA-Consulting/skia third_party/skia
+rmdir /s /q third_party/pdfium
+git clone -b gtx --depth=1 https://github.com/OSITA-Consulting/PDFium third_party/pdfium
+gclient runhooks 
+gn gen out/gtx --args="treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true" 
+autoninja -C out/gtx chrome 
+ninja -C out/gtx mini_installer 
+"out/gtx/gtxbrowser.exe" 
 
 
 
