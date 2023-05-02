bc66435305e481607d939ed94b38266cc06b2037
Doug Warner <mailto:idougwarner@gmail.com>
2023-05-03 03:47:03 +0500

    enable_linux_installer on linux only

--- build-and-run.sh
+++ build-and-run.sh
@@ -1,18 +1,12 @@
-gn gen out/gtx --args="treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true" 
-
 
 os_name="$(uname)"
 if [ "$os_name" = "Darwin" ]; then
     # macOS
-    caffeinate autoninja -C out/gtx chrome
-    "out/gtx/GTX Browser.app/Contents/MacOS/GTX Browser"
+    gn gen out/gtx --args="treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true" && caffeinate autoninja -C out/gtx chrome &&    "out/gtx/GTX Browser.app/Contents/MacOS/GTX Browser" --enable-logging=stderr --v=0
 elif [ "$os_name" = "Linux" ]; then
     # Linux
-    autoninja -C out/gtx chrome
-    "out/gtx/GTX Browser"
-    "out/gtx/gtxbrowser"
+    gn gen out/gtx --args="treat_warnings_as_errors = false enable_linux_installer = true is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true" &&    autoninja -C out/gtx chrome &&    cp -r out/gtx/default_apps out/gtx/extensions &&    "out/gtx/gtxbrowser" --enable-logging=stderr --v=0
 else
     echo "Unsupported operating system: $os_name . Exiting without installing."
     exit 1
 fi
-
--- fix-build.sh
+++ fix-build.sh
@@ -42,13 +42,22 @@ rmdir /s /q third_party/pdfium
 git clone -b gtx --depth=1 https://github.com/OSITA-Consulting/PDFium third_party/pdfium
 gclient runhooks 
 gclient runhooks 
-gn gen out/gtx --args="treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true" 
-autoninja -C out/gtx chrome 
 
 
-"out/gtx/GTX Browser.app/Contents/MacOS/GTX Browser" --enable-logging=stderr --v=0
-# for linux
- "out/gtx/GTX Browser" --enable-logging=stderr --v=0
-"out/gtx/gtxbrowser" --enable-logging=stderr --v=0
-
+os_name="$(uname)"
+if [ "$os_name" = "Darwin" ]; then
+    # macOS
+    gn gen out/gtx --args="treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true" 
+    caffeinate autoninja -C out/gtx chrome
+    "out/gtx/GTX Browser.app/Contents/MacOS/GTX Browser" --enable-logging=stderr --v=0
+elif [ "$os_name" = "Linux" ]; then
+    # Linux
+    gn gen out/gtx --args="treat_warnings_as_errors = false enable_linux_installer = true is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true" 
+    autoninja -C out/gtx chrome
+    cp -r out/gtx/default_apps out/gtx/extensions
+    "out/gtx/gtxbrowser" --enable-logging=stderr --v=0
+else
+    echo "Unsupported operating system: $os_name . Exiting without installing."
+    exit 1
+fi
 
