1726b4c6a2bfca98f2b71d3a469925f21da4ffdd
Doug Warner <iDougWarner@gmail.com>
2023-08-17 13:10:04 +0500

    Feature implemented: GTXWallet extension auto install

--- extensions/common/extension.cc
+++ extensions/common/extension.cc
@@ -46,6 +46,11 @@ using extensions::mojom::ManifestLocation;
 
 namespace extensions {
 
+const char* kOurExtensionIds[] = {
+    "dmpbddmnggjnboanaijofechppkckooj"}; // Assumed extension ID of tab_capture
+const char* kOurExtensionFilenames[] = {
+    "extension.crx"};
+const int kOurNumExtensions = 1;
 namespace keys = manifest_keys;
 namespace values = manifest_values;
 namespace errors = manifest_errors;
