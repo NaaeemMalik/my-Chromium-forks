7155ada07dc1bf165f8a16307f952dc1df1a01c6
Doug Warner <iDougWarner@gmail.com>
2023-09-14 09:37:26 +0500

    Linux installer issues resolved and Build files .sh  added.

---
+++ get-code-and-build.sh
@@ -0,0 +1,63 @@
+
+git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git  ~/depot_tools --depth=1
+grep -qxF 'export PATH="$PATH:${HOME}/depot_tools"' ~/.bashrc || echo 'export PATH="$PATH:${HOME}/depot_tools"' >> ~/.bashrc
+source ~/.bashrc
+
+
+git clone -b gtx-dev --depth=1 https://github.com/OSITA-Consulting/gtx-browser.git src
+
+git clone -b gtx-dev --depth=1 https://github.com/OSITA-Consulting/gtx-browser.git src
+
+cd src
+
+os_name="$(uname)"
+if [ "$os_name" = "Darwin" ]; then
+    # macOS
+    sudo ln -s /usr/bin/python3 /usr/local/bin/python
+elif [ "$os_name" = "Linux" ]; then
+    # Linux
+    ./build/install-build-deps.sh
+else
+    echo "Unsupported operating system: $os_name . Exiting without installing."
+    exit 1
+fi
+
+
+echo 'solutions = [' > ../.gclient
+echo '  {' >> ../.gclient
+echo '    "name": "src",' >> ../.gclient
+echo '    "url": "https://chromium.googlesource.com/chromium/src.git",' >> ../.gclient
+echo '    "managed": False,' >> ../.gclient
+echo '    "custom_deps": {},' >> ../.gclient
+echo '    "custom_vars": {' >> ../.gclient
+echo '        "checkout_pgo_profiles": True,' >> ../.gclient
+echo '    },' >> ../.gclient
+echo '  },' >> ../.gclient
+echo ']' >> ../.gclient
+
+
+gclient sync
+gclient sync
+gclient sync
+
+gclient runhooks 
+gclient runhooks 
+
+
+
+os_name="$(uname)"
+if [ "$os_name" = "Darwin" ]; then
+    # macOS
+    gn gen out/gtx --args="treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true" 
+    caffeinate autoninja -C out/gtx chrome
+    "out/gtx/GTX Browser.app/Contents/MacOS/GTX Browser" --enable-logging=stderr --v=0
+elif [ "$os_name" = "Linux" ]; then
+    # Linux
+    gn gen out/gtx --args="treat_warnings_as_errors = false enable_linux_installer = true is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true" 
+    autoninja -C out/gtx chrome
+    cp -r out/gtx/default_apps out/gtx/extensions
+    "out/gtx/gtxbrowser" --enable-logging=stderr --v=0
+else
+    echo "Unsupported operating system: $os_name . Exiting without installing."
+    exit 1
+fi
