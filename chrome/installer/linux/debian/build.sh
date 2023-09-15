616b453cca351dfe2fafa032abe28211a45e8706
Doug Warner <iDougWarner@gmail.com>
2023-09-15 12:17:46 +0500

    Linux installer code working properly

--- chrome/installer/linux/debian/build.sh
+++ chrome/installer/linux/debian/build.sh
@@ -121,7 +121,10 @@ do_package() {
   local PACKAGEFILE="${PACKAGE}-${CHANNEL}_${VERSIONFULL}_${ARCHITECTURE}.deb"
   if [ ${IS_OFFICIAL_BUILD} -ne 0 ]; then
     (cd "${TMPFILEDIR}" && ar -x "${TMPFILEDIR}/${PACKAGEFILE}")
-    xz -z9 -T0 --lzma2='dict=256MiB' "${TMPFILEDIR}/data.tar"
+    # xz -z9 -T0 --lzma2='dict=256MiB' "${TMPFILEDIR}/data.tar" 
+    # This dictionary size was causing problem of memory allocation so I changed it to following:
+    xz -z9 "${TMPFILEDIR}/data.tar
+    
     xz -z0 "${TMPFILEDIR}/control.tar"
     ar -d "${TMPFILEDIR}/${PACKAGEFILE}" control.tar data.tar
     ar -r "${TMPFILEDIR}/${PACKAGEFILE}" "${TMPFILEDIR}/control.tar.xz" \
