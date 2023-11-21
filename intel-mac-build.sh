a174d8ecad68cda9cdc64c47e13df31771832b8b
Doug Warner <iDougWarner@gmail.com>
2023-11-21 10:06:41 +0500

    fixed intel-mac-build.sh

--- intel-mac-build.sh
+++ intel-mac-build.sh
@@ -5,7 +5,7 @@ browser_name="GTx Browser"
 out_dir="out/intel"
 
 # Generate the build
-gn gen "${out_dir}" --args="treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true target_cpu=\"x64\"" && caffeinate autoninja -C "${out_dir}" chrome 
+gn gen "${out_dir}" --args="enable_widevine=true treat_warnings_as_errors = false is_debug=false dcheck_always_on=false blink_symbol_level=0 symbol_level=0 proprietary_codecs=true ffmpeg_branding=\"Chrome\" is_official_build=true target_cpu=\"x64\"" && caffeinate autoninja -C "${out_dir}" chrome 
 
 # Define base directories
 BASE_OUT_DIR="${out_dir}/${browser_name}.app/Contents"
